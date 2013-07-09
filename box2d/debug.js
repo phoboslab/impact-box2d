4de54518e62e7932b56f764be47e44b644e1e0d3
Dominic Szablewski <mail@phoboslab.org>
2013-07-09 23:26:06 +0200

    Updated debug drawer to draw semi-transparent. Thanks Joncom

--- box2d/debug.js
+++ box2d/debug.js
@@ -2,6 +2,7 @@ ig.module(
 	'plugins.box2d.debug'
 )
 .requires(
+	'impact.game',
 	'plugins.box2d.lib'
 )
 .defines(function(){
@@ -9,69 +10,76 @@ ig.module(
 
 ig.Box2DDebug = ig.Class.extend({
 	drawer: null,
+	canvas: null,
 	world: null,
-	alpha: 1,
-	
-	init: function( world, alpha, thickness ) {
-		this.world = world;
-		
-		this.drawer = new Box2D.Dynamics.b2DebugDraw();
-		this.drawer.SetSprite(ig.system.context);
 
+	alpha: 0.5,
+	thickness: 1.0,
+
+	init: function(world, alpha, thickness) {
+		this.world = world;
+		this.canvas = ig.system.canvas;
+		this.drawer = new Box2D.Dynamics.b2DebugDraw();
+		this.drawer.SetSprite(this);
 		this.drawer.SetDrawScale(1 / Box2D.SCALE * ig.system.scale);
-		this.drawer.SetFillAlpha(alpha || 0.3);
-		this.drawer.SetLineThickness(thickness || 1.0);
+		this.drawer.SetFillAlpha(alpha || this.alpha);
+		this.drawer.SetLineThickness(thickness || this.thickness);
 		this.drawer.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit | Box2D.Dynamics.b2DebugDraw.e_jointBit);
 		world.SetDebugDraw(this.drawer);
 	},
-	
+
 	draw: function() {
 		ig.system.context.save();
-		ig.system.context.translate( -ig.game.screen.x * ig.system.scale, -ig.game.screen.y * ig.system.scale );
-		
-		// Set debug drawer, draw and unset again, to prevent box2d
-		// from drawing on it's own during step()
-		this.world.SetDebugDraw( this.drawer );
+		ig.system.context.translate(-ig.game.screen.x * ig.system.scale, -ig.game.screen.y * ig.system.scale);
 		this.world.DrawDebugData();
-		this.world.SetDebugDraw( null );
-		
 		ig.system.context.restore();
 	},
 
-	
-	clear: function(){},
-	
-	lineStyle: function( thickness, color, alpha ) {
-		ig.system.context.strokeStyle = 'rgb('+color._r+','+color._g+','+color._b+')';
-		ig.system.context.lineWidth = thickness;
-	},
-	
-	moveTo: function( x, y ) {
-		ig.system.context.beginPath();
-		ig.system.context.moveTo( x, y );
-	},
-	
-	lineTo: function( x, y ) {
-		ig.system.context.lineTo( x, y );
-		ig.system.context.stroke();
-	},
-	
-	beginFill: function( color, alpha ) {
-		this.alpha = alpha;
-		ig.system.context.fillStyle = 'rgb('+color._r+','+color._g+','+color._b+')';
+	clearRect: function() {},
+
+	beginPath: function() {
+		ig.system.context.lineWidth = this.strokeWidth;
+		ig.system.context.fillStyle = this.fillStyle;
+		ig.system.context.strokeStyle = this.strokeSyle;
 		ig.system.context.beginPath();
 	},
-	
-	endFill: function() {
-		ig.system.context.globalAlpha = this.alpha;
+
+	arc: function(x, y, radius, startAngle, endAngle, counterClockwise) {
+		ig.system.context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+	},
+
+	closePath: function() {
+		ig.system.context.closePath();
+	},
+
+	fill: function() {
+		ig.system.context.fillStyle = this.fillStyle;
 		ig.system.context.fill();
-		ig.system.context.globalAlpha = 1;
 	},
-	
-	drawCircle: function( x, y, r ) {
-		ig.system.context.beginPath();
-		ig.system.context.arc(x, y, r, 0, Math.PI*2, true);
+
+	stroke: function() {
 		ig.system.context.stroke();
+	},
+
+	moveTo: function(x, y) {
+		ig.system.context.moveTo(x, y);
+	},
+
+	lineTo: function(x, y) {
+		ig.system.context.lineTo(x, y);
+		ig.system.context.stroke();
+	}
+
+});
+
+ig.Game.inject({
+	loadLevel: function(data) {
+		this.parent(data);
+		this.debugDrawer = new ig.Box2DDebug(ig.world);
+	},
+	draw: function() {
+		this.parent();
+		this.debugDrawer.draw();
 	}
 });
 
